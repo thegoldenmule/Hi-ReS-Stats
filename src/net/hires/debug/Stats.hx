64f94d6f2d1d877d34d0be0f74699807cd914981
Benjamin Jordan <bjordan@electrotank.com>
2011-08-05 15:06:49 -0700

    fixing haxe implementation

--- src/net/hires/debug/Stats.hx
+++ src/net/hires/debug/Stats.hx
@@ -20,10 +20,10 @@ import flash.events.Event;
 import flash.events.MouseEvent;
 import flash.geom.Matrix;
 import flash.geom.Rectangle;
+import flash.Lib;
 import flash.system.System;
 import flash.text.StyleSheet;
 import flash.text.TextField;
-import flash.xml.XML;
 
 
 class Stats extends Sprite {	
@@ -32,8 +32,6 @@ class Stats extends Sprite {
 	static inline var XPOS : Int = 69;//width - 1
 	static inline var GRAPH_HEIGHT : Int = 50;
 	static inline var TEXT_HEIGHT : Int = 50;
-
-	private var xml : XML;
 
 	private var text : TextField;
 	private var style : StyleSheet;
@@ -58,14 +56,10 @@ class Stats extends Sprite {
 	 * <b>Stats</b> FPS, MS and MEM, all in one.
 	 */
 	public function new() {
-
 		super();
 		mem_max = 0;
 		fps = 0;
-
-
-		xml = new XML("<xml><fps>FPS:</fps><ms>MS:</ms><mem>MEM:</mem><memMax>MAX:</memMax></xml>");
-
+		
 		style = new StyleSheet();
 		style.setStyle('xml', {fontSize:'9px', fontFamily:'_sans', leading:'-2px'});
 		style.setStyle('fps', {color: Colors.fpsCSS });
@@ -82,60 +76,54 @@ class Stats extends Sprite {
 		text.mouseEnabled = false;
 		
 		rectangle = new Rectangle(GRAPH_WIDTH - 1, 0, 1, GRAPH_HEIGHT);			
-
-		this.addEventListener(Event.ADDED_TO_STAGE, init, false, 0, true);
-		this.addEventListener(Event.REMOVED_FROM_STAGE, destroy, false, 0, true);
 		
+		addEventListener(Event.ADDED_TO_STAGE, init, false, 0, true);
+		addEventListener(Event.REMOVED_FROM_STAGE, destroy, false, 0, true);
 	}
 
-	private function init(e : Event) {
-
-		_stage = flash.Lib.current.stage;
+	private function init(event:Event) {
+		_stage = Lib.current.stage;
+		
 		graphics.beginFill(Colors.bg);
 		graphics.drawRect(0, 0, GRAPH_WIDTH, TEXT_HEIGHT);
 		graphics.endFill();
-
-		this.addChild(text);
+		
+		addChild(text);
 		
 		graph = new BitmapData(GRAPH_WIDTH, GRAPH_HEIGHT, false, Colors.bg);
 		graphics.beginBitmapFill(graph, new Matrix(1, 0, 0, 1, 0, TEXT_HEIGHT));
 		graphics.drawRect(0, TEXT_HEIGHT, GRAPH_WIDTH, GRAPH_HEIGHT);
-
-		this.addEventListener(Event.ENTER_FRAME, update);
 		
+		addEventListener(Event.ENTER_FRAME, update);
 	}
 
-	private function destroy(e : Event) {
-		
+	private function destroy(event:Event) {
 		graphics.clear();
 		
-		while(numChildren > 0)
-			removeChildAt(0);			
+		while(numChildren > 0) removeChildAt(0);			
 		
 		graph.dispose();
 		
 		removeEventListener(Event.ENTER_FRAME, update);
-		
 	}
 
 	private function update(e : Event) {
-
-		timer = flash.Lib.getTimer();
+		timer = Lib.getTimer();
 		
 		//after a second has passed 
 		if( timer - 1000 > ms_prev ) {
-
 			mem = System.totalMemory * 0.000000954;
 			mem_max = mem_max > mem ? mem_max : mem;
-
+			
 			fps_graph = GRAPH_HEIGHT - Std.int( Math.min(GRAPH_HEIGHT, ( fps / _stage.frameRate ) * GRAPH_HEIGHT) );
-
+			
 			mem_graph = GRAPH_HEIGHT - normalizeMem(mem);
 			mem_max_graph = GRAPH_HEIGHT - normalizeMem(mem_max);
+			
 			//milliseconds since last frame -- this fluctuates quite a bit
 			ms_graph = Std.int( GRAPH_HEIGHT - ( ( timer - ms ) >> 1 ));
 			graph.scroll(-1, 0);
-
+			
 			graph.fillRect(rectangle, Colors.bg);
 			graph.lock();
 			graph.setPixel(XPOS, fps_graph, Colors.fps);
@@ -143,24 +131,23 @@ class Stats extends Sprite {
 			graph.setPixel(XPOS, mem_max_graph, Colors.memmax);
 			graph.setPixel(XPOS, ms_graph, Colors.ms);
 			graph.unlock();
-
-			xml.fps = "FPS: " + fps + " / " + stage.frameRate; 
-			xml.mem = "MEM: " + mem;
-			xml.memMax = "MAX: " + mem_max;			
-
+			
+			text.htmlText = 
+				"<fps>FPS: " + fps + " / " + stage.frameRate + "</fps>" +
+				"<ms>MS: " + (timer - ms) + "</ms>" +
+				"<mem>MEM: " + mem + "</mem>" +
+				"<memMax>MAX: " + mem_max + "</memMax>";
+			
 			//reset frame and time counters
 			fps = 0;
 			ms_prev = timer;
-
+			
 			return;
 		}
+		
 		//increment number of frames which have occurred in current second
 		fps++;
-
-		xml.ms = "MS: " + (timer - ms);
 		ms = timer;
-		
-		text.htmlText = xml.toString();
 	}
 
 
